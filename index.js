efe0f209b217d56987b2f07744cad6c2bb74aa17
Alexis S Liu <syl1350@gmail.com>
2021-07-19 10:59:42 -0400

    commit tree 456a6a498ae58c75270510f5cc5e45e3825b0f2f

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 2;
+let num2 = 31;
+let num3 = 9;
+let num4 = 5;
 
-// Write your code here
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random())+ 1;
+
+const mod = num3 % num4;
+
+const max = Math.max(1, 20)
